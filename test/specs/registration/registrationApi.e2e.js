af43b589b9a79f0232456bdc17558d5a0e633f9b
Aleksandr <aik5787@gmail.com>
2024-11-25 14:11:19 -0500

    tests are added

--- test/specs/registration/registrationApi.e2e.js
+++ test/specs/registration/registrationApi.e2e.js
@@ -1,21 +1,21 @@
-// import registrationApi from "../../api/rigistrationApi.js";
+import registrationApi from "../../api/rigistrationApi.js";
 import loginApi from "../../api/loginApi.js";
 // import deleteUserApi from "../../api/deleteUserApi.js";
-// import { faker } from "@faker-js/faker";
+import { faker } from "@faker-js/faker";
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-// const email = faker.internet.email();
-// const password = faker.internet.password();
-// const userName = faker.internet.username();
-// const userSurname = faker.internet.username();
-// let userId;
+const email = faker.internet.email();
+const password = faker.internet.password();
+const userName = faker.internet.username();
+const userSurname = faker.internet.username();
+let userId;
 
 describe("Registration", () => {
 
   it("should register a new account via A PI @regression", async () => {
-    // userId = await registrationApi.registerNewUser(userName, userSurname, email, password);
+    userId = await registrationApi.registerNewUser(userName, userSurname, email, password);
     const adminAccessToken = await loginApi.login(process.env.ADMINEMAIL, process.env.ADMINPASSWORD);
     // await deleteUserApi.deleteUser(userId, adminAccessToken);
   });
