2bec66208829433756b390001650b90d63fd41b8
Aleksandr <aik5787@gmail.com>
2024-11-25 14:12:46 -0500

    tests are added

--- test/specs/registration/registrationApi.e2e.js
+++ test/specs/registration/registrationApi.e2e.js
@@ -1,6 +1,6 @@
 import registrationApi from "../../api/rigistrationApi.js";
 import loginApi from "../../api/loginApi.js";
-// import deleteUserApi from "../../api/deleteUserApi.js";
+import deleteUserApi from "../../api/deleteUserApi.js";
 import { faker } from "@faker-js/faker";
 import dotenv from 'dotenv';
 
@@ -17,6 +17,6 @@ describe("Registration", () => {
   it("should register a new account via A PI @regression", async () => {
     userId = await registrationApi.registerNewUser(userName, userSurname, email, password);
     const adminAccessToken = await loginApi.login(process.env.ADMINEMAIL, process.env.ADMINPASSWORD);
-    // await deleteUserApi.deleteUser(userId, adminAccessToken);
+    await deleteUserApi.deleteUser(userId, adminAccessToken);
   });
 });
