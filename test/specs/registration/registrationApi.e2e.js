b1d997d376a7229348462b5e72e4b5b19c79d15e
Aleksandr <aik5787@gmail.com>
2024-11-25 14:09:39 -0500

    tests are added

--- test/specs/registration/registrationApi.e2e.js
+++ test/specs/registration/registrationApi.e2e.js
@@ -1,16 +1,16 @@
-import registrationApi from "../../api/rigistrationApi.js";
+// import registrationApi from "../../api/rigistrationApi.js";
 import loginApi from "../../api/loginApi.js";
-import deleteUserApi from "../../api/deleteUserApi.js";
-import { faker } from "@faker-js/faker";
+// import deleteUserApi from "../../api/deleteUserApi.js";
+// import { faker } from "@faker-js/faker";
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const email = faker.internet.email();
-const password = faker.internet.password();
-const userName = faker.internet.username();
-const userSurname = faker.internet.username();
-let userId;
+// const email = faker.internet.email();
+// const password = faker.internet.password();
+// const userName = faker.internet.username();
+// const userSurname = faker.internet.username();
+// let userId;
 
 describe("Registration", () => {
 
--- test/specs/registration/registrationUiPositive.e2e.js
+++ test/specs/registration/registrationUiPositive.e2e.js
@@ -1,49 +1,49 @@
-import { faker } from "@faker-js/faker";
-import dashboardPage from "../../pageobjects/dashboard.page.js";
-import registrationPage from "../../pageobjects/registration.page.js";
-import userCredentials from "../../fixture/userCredentials.json";
-import loginApi from "../../api/loginApi.js";
-import deleteUserApi from "../../api/deleteUserApi.js";
-import dotenv from "dotenv";
+// import { faker } from "@faker-js/faker";
+// import dashboardPage from "../../pageobjects/dashboard.page.js";
+// import registrationPage from "../../pageobjects/registration.page.js";
+// import userCredentials from "../../fixture/userCredentials.json";
+// import loginApi from "../../api/loginApi.js";
+// import deleteUserApi from "../../api/deleteUserApi.js";
+// import dotenv from "dotenv";
 
-dotenv.config();
+// dotenv.config();
 
-const email = faker.internet.email();
-const password = faker.internet.password();
-const firstName = faker.internet.username();
-const lastName = faker.internet.username();
+// const email = faker.internet.email();
+// const password = faker.internet.password();
+// const firstName = faker.internet.username();
+// const lastName = faker.internet.username();
 
-describe("Registration Positive scenario", () => {
-  beforeEach(async () => {
-    await browser.url("/auth/register");
-  });
+// describe("Registration Positive scenario", () => {
+//   beforeEach(async () => {
+//     await browser.url("/auth/register");
+//   });
 
-  after(async () => {
-    const adminAccessToken = await loginApi.login(process.env.ADMINEMAIL, process.env.ADMINPASSWORD);
-    const userId = await browser.sharedStore.get("userId");
-    await deleteUserApi.deleteUser(userId, adminAccessToken);
-  });
+//   after(async () => {
+//     const adminAccessToken = await loginApi.login(process.env.ADMINEMAIL, process.env.ADMINPASSWORD);
+//     const userId = await browser.sharedStore.get("userId");
+//     await deleteUserApi.deleteUser(userId, adminAccessToken);
+//   });
 
-  it("Should register a new account via UI", async () => {
-    await browser.setupInterceptor();
+//   it("Should register a new account via UI", async () => {
+//     await browser.setupInterceptor();
 
-    await registrationPage.registration(firstName, lastName, email, password);
-    await browser.pause(3000);
+//     await registrationPage.registration(firstName, lastName, email, password);
+//     await browser.pause(3000);
 
-    const requests = await browser.getRequests();
-    const request = requests.find((req) => req.url.includes("api/users/registration"));
+//     const requests = await browser.getRequests();
+//     const request = requests.find((req) => req.url.includes("api/users/registration"));
 
-    const userId = request.response.body.user.id;
-    await browser.sharedStore.set("userId", userId);
-    // const accessToken = request.response.body.accessToken;
-    // await browser.execute((token) => {
-    //   localStorage.setItem("accessToken", token);
-    // }, accessToken);
-    await dashboardPage.roleLbl.waitForDisplayed({ timeout: 15000 });
-    await expect(dashboardPage.roleLbl).toHaveText(userCredentials.user.role);
-    await dashboardPage.nameLbl.waitForDisplayed({ timeout: 15000 });
-    await expect(dashboardPage.nameLbl).toHaveText(`${firstName} ${lastName}`);
-    await expect(browser).toHaveTitle("User: Profile | Delek Homes");
-  });
+//     const userId = request.response.body.user.id;
+//     await browser.sharedStore.set("userId", userId);
+//     // const accessToken = request.response.body.accessToken;
+//     // await browser.execute((token) => {
+//     //   localStorage.setItem("accessToken", token);
+//     // }, accessToken);
+//     await dashboardPage.roleLbl.waitForDisplayed({ timeout: 15000 });
+//     await expect(dashboardPage.roleLbl).toHaveText(userCredentials.user.role);
+//     await dashboardPage.nameLbl.waitForDisplayed({ timeout: 15000 });
+//     await expect(dashboardPage.nameLbl).toHaveText(`${firstName} ${lastName}`);
+//     await expect(browser).toHaveTitle("User: Profile | Delek Homes");
+//   });
 
-});
+// });
